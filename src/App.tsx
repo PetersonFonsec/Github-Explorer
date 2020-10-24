34065f259fd42da99990f403ab357a05011728a3
Peterson <petersonsimiao1@gmail.com>
2020-10-24 18:45:50 -0300

    feat: criado o css global da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-
+import GlobalStyle from './styles/global';
 import Routes from './routes/index';
 
 const App: React.FC = () => (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyle />
+  </>
 );
 
 export default App;
---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components';
+import githubBackground from '../assets/github.svg';
+
+export default createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    outline: 0;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+    background: #E5E5E5 url(${githubBackground}) no-repeat 70% top;
+    font-family: 'Roboto', sans-serif;
+    font-size: 16px;
+  }
+
+  #root {
+    max-width: 960px;
+    margin: 0 auto;
+    padding: 40px 20px;
+  }
+`;
