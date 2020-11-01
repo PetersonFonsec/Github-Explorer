3e2582ddf65c6384b1fe47fce5a8b1ca2abff806
Peterson <petersonsimiao1@gmail.com>
2020-11-01 17:50:13 -0300

    fix: ajustado arquivos de rotas

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,22 +1,19 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Dashboard from '../views/Dashboard.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Dashboard',
+    component: Dashboard
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/repositorio',
+    name: 'Repository',
+    component: () => import(/* webpackChunkName: "repository" */ '../views/Repository.vue')
   }
 ]
 
---
+++ vue/src/views/Dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home"></div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: "Dashboard",
+  components: {
+    HelloWorld,
+  },
+};
+</script>
